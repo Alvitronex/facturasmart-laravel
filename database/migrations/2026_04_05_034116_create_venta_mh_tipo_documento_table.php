f1762e373214bb89c7327e59aeb6a4b7572aa3b7
alvitronex <marioenriquealvarado12@gmail.com>
2026-04-04 21:42:12 -0600

    creando tabla tipo de documento

---
+++ database/migrations/2026_04_05_034116_create_venta_mh_tipo_documento_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venta_mh_tipo_documento', function (Blueprint $table) {
+            $table->id('id_tipo_documento');
+            $table->string('tipo_documento');
+            $table->string('cod_tipo_documento');
+            $table->string('estado')->default('1');
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('venta_mh_tipo_documento');
+    }
+};
